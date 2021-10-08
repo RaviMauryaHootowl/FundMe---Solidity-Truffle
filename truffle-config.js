d378d83d264aaf693aee9347fbc8e48ac9e5ee57
RaviMauryaHootowl <ravimaurya027@gmail.com>
2021-10-08 18:41:52 +0530

    rinkeby test network added

--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,8 @@
 const path = require("path");
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+require('dotenv').config()
+const mnemonic = process.env.MNEMONIC;
+const infura_url = "https://rinkeby.infura.io/v3/" + process.env.INFURA_API_KEY;
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -7,6 +11,12 @@ module.exports = {
   networks: {
     develop: {
       port: 8545
+    },
+    rinkeby: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, infura_url)
+      },
+      network_id: 4
     }
   }
 };
